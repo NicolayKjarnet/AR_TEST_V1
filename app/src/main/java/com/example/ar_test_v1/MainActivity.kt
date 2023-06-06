3ce5bddff65e0ff5511cff14255e7794604e632d
Faisal Omar <93327868+faom002@users.noreply.github.com>
2023-06-06 12:44:29 +0200

    update

--- app/src/main/java/com/example/ar_test_v1/MainActivity.kt
+++ app/src/main/java/com/example/ar_test_v1/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.ar_test_v1
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isGone
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import io.github.sceneview.ar.ArSceneView
@@ -12,6 +20,8 @@ import io.github.sceneview.ar.arcore.LightEstimationMode
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.PlacementMode
 import io.github.sceneview.math.Position
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -80,5 +90,38 @@ class MainActivity : AppCompatActivity() {
             "Random sentence generator"
         )
         return sentences.random()
+    }
+
+    fun screenshot(view: View) {
+        val rootView = window.decorView.rootView
+        val bitmap = getBitmapFromView(rootView)
+
+        try {
+            val contentResolver = applicationContext.contentResolver
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, "screenshot.png")
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            }
+
+            val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            val outputStream = imageUri?.let { contentResolver.openOutputStream(it) }
+
+            outputStream?.use {
+                bitmap?.compress(Bitmap.CompressFormat.PNG, 100, it)
+                Toast.makeText(this, "Screenshot saved", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Failed to save screenshot", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap? {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(Color.WHITE)
+        view.draw(canvas)
+        return bitmap
     }
 }
