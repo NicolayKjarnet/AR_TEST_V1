3d0bbf77ad3acec4b93761d64c31ddb14d060027
Faisal Omar <93327868+faom002@users.noreply.github.com>
2023-06-06 11:13:28 +0200

    unplace anchor

--- app/src/main/java/com/example/ar_test_v1/MainActivity.kt
+++ app/src/main/java/com/example/ar_test_v1/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.ar_test_v1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import android.widget.FrameLayout
-import android.widget.RelativeLayout
-
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isGone
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
-import dev.romainguy.kotlin.math.pointAt
-import dev.romainguy.kotlin.math.pow
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.arcore.LightEstimationMode
 import io.github.sceneview.ar.node.ArModelNode
@@ -22,9 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var sceneView: ArSceneView
     lateinit var placeButton: ExtendedFloatingActionButton
+    lateinit var unAnchorButton: Button
     private lateinit var modelNode: ArModelNode
     lateinit var textModel: TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         placeButton = findViewById(R.id.place)
-
+        unAnchorButton = findViewById(R.id.resetAnchor)
         textModel = findViewById(R.id.text)
 
         placeButton.setOnClickListener {
@@ -43,22 +38,24 @@ class MainActivity : AppCompatActivity() {
             textModel.text = generateRandomSentence()
         }
 
+        unAnchorButton.setOnClickListener {
+            unAnchorModel()
+        }
+
         modelNode = ArModelNode(PlacementMode.INSTANT).apply {
             loadModelGlbAsync(
                 glbFileLocation = "models/mario.glb",
                 scaleToUnits = 1f,
                 centerOrigin = Position(-0.5f)
-
-
             ) {
                 sceneView.planeRenderer.isVisible = true
             }
             onAnchorChanged = {
                 placeButton.isGone = it != null
+                unAnchorButton.isGone = it == null
             }
         }
         sceneView.addChild(modelNode)
-
     }
 
     @SuppressLint("SetTextI18n")
@@ -66,6 +63,12 @@ class MainActivity : AppCompatActivity() {
         modelNode.anchor()
         sceneView.planeRenderer.isVisible = false
         textModel.text = "hello"
+    }
+
+    private fun unAnchorModel() {
+        sceneView.removeChild(modelNode)
+        sceneView.planeRenderer.isVisible = true
+        textModel.text = ""
     }
 
     private fun generateRandomSentence(): String {
